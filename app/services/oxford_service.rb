4a60821a40e6cb105fa147c4a78f9d52231996c3
ClaireBvs <clairembeauvais@gmail.com>
2018-10-30 12:26:13 -0600

    Finishing refactor to add OxfordService [#1]

--- app/services/oxford_service.rb
+++ app/services/oxford_service.rb
@@ -1,23 +1,19 @@
 class OxfordService
+  attr_reader :word
+
   def initialize(word)
     @word = word
   end
 
-  def sentences_british_canadian
-    conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
-      faraday.headers["app_id"] = ENV['oxford_api_id']
-      faraday.headers["app_key"] = ENV['oxford_api_key']
-      faraday.adapter  Faraday.default_adapter
-    end
-
-    response = conn.get("api/v1/entries/en/strong/sentences")
-
-    sentences_data = JSON.parse(response.body, symbolize_names: true)[:results][0][:lexicalEntries][0][:sentences].map do |sentence|
+  def sentences_data
+    JSON.parse(response.body, symbolize_names: true)[:results][0][:lexicalEntries][0][:sentences].map do |sentence|
       if sentence[:regions] == ["British"] || sentence[:regions] == ["Canadian"]
         sentence
       end
     end
+  end
 
+  def sentences_british_canadian
     array_sentences_brit_can = []
     sentences_data.map do |sentence|
       if sentence != nil
@@ -26,4 +22,18 @@ class OxfordService
     end
     return array_sentences_brit_can
   end
+
+  def response
+    conn.get("api/v1/entries/en/#{@word}/sentences")
+  end
+
+  private
+  def conn
+    Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
+      faraday.headers["app_id"] = ENV['oxford_api_id']
+      faraday.headers["app_key"] = ENV['oxford_api_key']
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
+
 end
