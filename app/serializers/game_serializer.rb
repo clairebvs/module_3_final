4ff6c942a81e678fef36a593b9ffe6c33cc241fc
ClaireBvs <clairembeauvais@gmail.com>
2018-10-30 11:24:39 -0600

    Add #player_1 and #player_2 for gameserializer [#2]

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -6,5 +6,24 @@ class GameSerializer < ActiveModel::Serializer
   end
 
   def scores
+    [
+      {
+        user_id: player_1.id,
+        score: player_1.score
+      },
+      {
+        user_id: player_2.id,
+        score: player_2.score
+      }
+    ]
+  end
+
+  private
+  def player_1
+    User.find(object.player_1_id)
+  end
+
+  def player_2
+    User.find(object.player_2_id)
   end
 end
