0fdd2f2be188d5bf46648f6905a1662037ef6b08
ClaireBvs <clairembeauvais@gmail.com>
2018-10-30 12:03:16 -0600

    Refactor WordFacade with #service [#1]

--- app/facades/word_facade.rb
+++ app/facades/word_facade.rb
@@ -6,34 +6,14 @@ class WordFacade
   end
 
   def sentences
-    # conn = Faraday.new(url: "https://od-api.oxforddictionaries.com") do |faraday|
-    #   faraday.headers["app_id"] = ENV['oxford_api_id']
-    #   faraday.headers["app_key"] = ENV['oxford_api_key']
-    #   faraday.adapter  Faraday.default_adapter
-    # end
-    #
-    # response = conn.get("api/v1/entries/en/strong/sentences")
-    #
-    # sentences_data = JSON.parse(response.body, symbolize_names: true)[:results][0][:lexicalEntries][0][:sentences].map do |sentence|
-    #   if sentence[:regions] == ["British"] || sentence[:regions] == ["Canadian"]
-    #     sentence
-    #   end
-    # end
-    #
-    # array_sentences_brit_can = []
-    # sentences_data.map do |sentence|
-    #   if sentence != nil
-    #     array_sentences_brit_can << sentence
-    #   end
-    # end
-    @service = OxfordService.new(word)
-
-    @service.sentences_british_canadian.map do |sentence_data|
+    service.sentences_british_canadian.map do |sentence_data|
       Sentence.new(sentence_data)
     end
+  end
 
-    # array_sentences_brit_can.map do |sentence_data|
-    #   Sentence.new(sentence_data)
-    # end
+  private
+
+  def service
+    OxfordService.new(word)
   end
 end
