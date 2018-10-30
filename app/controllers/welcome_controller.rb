70794979a6c8770a8dea04f61b824813344a5e75
ClaireBvs <clairembeauvais@gmail.com>
2018-10-30 09:42:50 -0600

    Add @wordfacade to welcome controller [#1]

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    # require "pry"; binding.pry
+    @word_facade = WordFacade.new(word_params[:word])
+  end
+
+  private
+
+  def word_params
+    params.permit(:word)
   end
 end
